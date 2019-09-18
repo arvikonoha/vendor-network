59ade709ae63af34c4e144a6cbd80db7ce54eb2a
arvikonoha <arvikonoha@gmail.com>
2019-09-18 20:58:15 +0530

    Change to welcome text

--- src/components/home/welcome-text/WelcomeText.jsx
+++ src/components/home/welcome-text/WelcomeText.jsx
@@ -5,7 +5,7 @@ function WelcomeText(){
   return (
     <div className="welcome-text">
       <h2>Welcome</h2>
-      <p>One place for all the event management needs. Find the perfect service as per your need as per your required quality,location,budget and so on</p>
+      <p>One place for all the event management needs. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nisi earum ad ea ipsum totam.</p>
     </div>
   )
 }
